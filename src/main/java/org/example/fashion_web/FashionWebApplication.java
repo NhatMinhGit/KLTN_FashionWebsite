b1644b6c17b19853a9af596ed37ac04a9b04f82e
Nguyễn Trần Nhật Minh <92421541+NhatMinhGit@users.noreply.github.com>
2025-01-20 20:19:23 +0700

    Initial commit for NguyenTranNhatMinh branch

---
+++ src/main/java/org/example/fashion_web/FashionWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.fashion_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/fashion_web/FashionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fashion_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
